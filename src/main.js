cbecb6b91c56792c41e38c9d59537bc3de19e7b6
fan379660107 <379660107@qq.com>
2022-06-27 20:42:46 +0800

    加载器 - 处理css文件

--- src/main.js
+++ src/main.js
@@ -4,3 +4,5 @@ import './tabs.js'
 
 import $ from 'jquery';
 $('#swiper').css('background', 'red')
+
+import './styles/index.css'
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,19 @@ module.exports = {
       template: './public/index.html', // 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
       filename: 'index.html', // 生成文件的名称
     }),
-    new CleanWebpackPlugin(),
+    // new CleanWebpackPlugin(),
   ],
+  module: {
+    // loader 加载器 配置在这儿
+    rules: [
+      // loader的规则
+      {
+        test: /\.css$/, // 匹配所有的css文件
+        // loader 执行的顺序： use数组里从右向左运行
+        // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+        // 再用 style-loader 将样式, 把css插入到dom中
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
