57578bea2172a73f99cedd08e1a7e8dabcd9cec5
fan379660107 <379660107@qq.com>
2022-06-27 21:20:44 +0800

    加载器 - 处理高版本js语法

--- src/main.js
+++ src/main.js
@@ -16,3 +16,14 @@ document.body.appendChild(img);
 
 // 引入字体图标文件
 import './assets/fonts/iconfont.css'
+
+
+class App {
+    static a = 123
+  }
+  
+  console.log(App.a)
+
+  const fn = () => {
+    console.log(111);
+  }
