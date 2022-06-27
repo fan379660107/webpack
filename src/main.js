77b2994eca44a46e137d07613bd13fe736fd7346
fan379660107 <379660107@qq.com>
2022-06-27 20:53:51 +0800

    加载器 - 处理图片

--- src/main.js
+++ src/main.js
@@ -7,3 +7,9 @@ $('#swiper').css('background', 'red')
 
 import './styles/index.css'
 import './styles/index.less';
+
+import imgUrl from './assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
